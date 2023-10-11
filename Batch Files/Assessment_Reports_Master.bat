7cab0be2becc477900c97e54037c222f5bd1527b
Phil Baranyai <23487048+pbaranyai@users.noreply.github.com>
2023-10-11 19:18:33 -0400

    Update Assessment_Reports_Master.bat

--- Batch Files/Assessment_Reports_Master.bat
+++ Batch Files/Assessment_Reports_Master.bat
@@ -1,14 +1,14 @@
-echo. > R:\GIS\GIS_LOGS\BatchLogs\Assessment_Reports_Master_bat.log
+echo. > \\FILELOCATION\GIS\GIS_LOGS\BatchLogs\Assessment_Reports_Master_bat.log
 ::  Comment Line
 :: 
 date=date /t
 time=time /t
 set STARTTIME=%TIME%
 ::
-Set wrkspce=R:\GIS\ArcAutomations\Assessment\Python
-::Set wrkspce=R:\GIS\ArcAutomations\Assessment\Python
+Set wrkspce=\\FILELOCATION\GIS\ArcAutomations\Assessment\Python
+::Set wrkspce=\\FILELOCATION\GIS\ArcAutomations\Assessment\Python
 ::
-Set batLogwrkspce=R:\GIS\GIS_LOGS\BatchLogs
+Set batLogwrkspce=\\FILELOCATION\GIS\GIS_LOGS\BatchLogs
 ::
 Set batLog=%batLogwrkspce%\Assessment_Reports_Master_bat.log
 ::
