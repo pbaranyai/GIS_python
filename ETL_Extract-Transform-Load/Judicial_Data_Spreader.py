af1366fa3417e82984945e216c5282d771659d5b
Phil Baranyai <23487048+pbaranyai@users.noreply.github.com>
2023-10-11 19:38:18 -0400

    Update Judicial_Data_Spreader.py

--- ETL_Extract-Transform-Load/Judicial_Data_Spreader.py
+++ ETL_Extract-Transform-Load/Judicial_Data_Spreader.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
 # Judicial_Data_Spreader.py
 # Created on: 2020-01-14 
@@ -18,14 +17,13 @@ import sys
 import arcpy
 import datetime
 import os
-import traceback
 import logging
 
 # Stop geoprocessing log history in metadata (stops program from filling up geoprocessing history in metadata with every run)
 arcpy.SetLogHistory(False)
 
 # Setup error logging (configure logging location, type, and filemode -- overwrite every run)
-logfile = r"R:\\GIS\\GIS_LOGS\\GIS\\Judicial_Data_Spreader.log"  
+logfile = r"\\FILELOCATION\\GIS\\GIS_LOGS\\GIS\\Judicial_Data_Spreader.log"  
 logging.basicConfig(filename= logfile, filemode='w', level=logging.DEBUG)
 
 # Setup Date (and day/time)
@@ -45,7 +43,7 @@ except:
     sys.exit ()
 
 #Database Connection Folder
-Database_Connections = r"\\CCFILE\\anybody\\GIS\\ArcAutomations\\Database_Connections"
+Database_Connections = r"\\FILELOCATION\\GIS\\ArcAutomations\\Database_Connections"
 
 # Database variables:
 CRAW_INTERNAL = Database_Connections + "\\craw_internal@ccsde.sde"
