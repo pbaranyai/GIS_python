7335b14c4d8eaae9f171b2dc48996a0467fd7035
Phil Baranyai <23487048+pbaranyai@users.noreply.github.com>
2024-03-28 12:11:24 -0400

    Update Religon_Data_Spreader.py

--- ETL_Extract-Transform-Load/Religon_Data_Spreader.py
+++ ETL_Extract-Transform-Load/Religon_Data_Spreader.py
@@ -13,12 +13,7 @@
 # ---------------------------------------------------------------------------
 
 # Import modules
-import sys
-import arcpy
-import datetime
-import os
-import traceback
-import logging
+import sys,arcpy,datetime,logging,time
 
 # Stop geoprocessing log history in metadata (stops program from filling up geoprocessing history in metadata with every run)
 arcpy.SetLogHistory(False)
@@ -40,7 +35,6 @@ try:
         return
 except:
     print ("\n Unable to write log file")
-    write_log("Unable to write log file", logfile)
     sys.exit ()
 
 #Database Connection Folder
