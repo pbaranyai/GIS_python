e4fb8dbdf1a264c575bfcf7cb34933fca540bb98
Phil Baranyai <23487048+pbaranyai@users.noreply.github.com>
2024-03-28 12:16:34 -0400

    Update ArcPro_Python_Checker.py

--- Miscellaneous/ArcPro_Python_Checker.py
+++ Miscellaneous/ArcPro_Python_Checker.py
@@ -1,15 +1,12 @@
 #Works in ArcGIS Pro
 
-import arcpy
+import arcpy,sys
 
 Tool = '\\FILELOCATION\GIS\ArcAutomations\GIS_Dept\Python\TopologyExport.py'
 Report = '\\FILELOCATION\GIS\ArcAutomations\GIS_Dept\ArcProChecker.txt'
 
 arcpy.AnalyzeToolsForPro_management(Tool, Report)
-
 print(arcpy.GetMessages(1))
+print ("  Report has been generated at:"+Report )
 
-print ("  Report has been generated")
-
-del arcpy
 sys.exit()    
